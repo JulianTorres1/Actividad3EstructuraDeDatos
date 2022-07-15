bb45de931ee893906412059087658af40affcf3f
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2022-07-15 10:31:57 -0500

    pilas tipo stack

--- Trabajo3Unidad3/src/main/java/com/mycompany/trabajo3unidad3/PilasTipoStack.java
+++ Trabajo3Unidad3/src/main/java/com/mycompany/trabajo3unidad3/PilasTipoStack.java
@@ -1,4 +1,27 @@
 package com.mycompany.trabajo3unidad3;
 
+import java.util.Stack;
+
 public class PilasTipoStack {
+    public static void main(String[] args) {
+        System.out.println("""
+                Que Desea Realizar:
+                    1. nada jaja
+                """);
+
+        Stack pila = new Stack();
+        pila.push(1);
+        pila.push(2);
+        pila.push(3);
+
+        System.out.println(pila);
+
+        pila.push(4);
+
+        System.out.println(pila);
+
+        pila.pop();
+
+
+    }
 }
