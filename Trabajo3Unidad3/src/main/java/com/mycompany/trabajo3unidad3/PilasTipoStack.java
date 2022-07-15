8d83503da8cc8e787c1bde240ac62e68eeadbf0e
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2022-07-15 11:40:41 -0500

    comenzando con la interfaz cli

--- Trabajo3Unidad3/src/main/java/com/mycompany/trabajo3unidad3/PilasTipoStack.java
+++ Trabajo3Unidad3/src/main/java/com/mycompany/trabajo3unidad3/PilasTipoStack.java
@@ -1,26 +1,51 @@
 package com.mycompany.trabajo3unidad3;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class PilasTipoStack {
     public static void main(String[] args) {
-        System.out.println("""
-                Que Desea Realizar:
-                    1. nada jaja
-                """);
+
+        //Variables
+
+        boolean on = true;
+        int opcion = 0;
+        //Objetos
 
         Stack pila = new Stack();
+
+        Scanner sc = new Scanner(System.in);
+
+        //elementos PreCargados
         pila.push(1);
         pila.push(2);
         pila.push(3);
+        System.out.println("Elementos Pre-Cargados: " + pila.peek());
 
-        System.out.println(pila);
 
-        pila.push(4);
+        while (on){
 
-        System.out.println(pila);
+            System.out.println("""
+                Que Desea Realizar:
+                    1. Ver Pila
+                    2. (Push) Ingresar dato a la pila
+                    3. (Pop ) sacar ultimo dato de la pila
+                """);
 
-        pila.pop();
+            opcion = sc.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Contenido: " + pila);
+            } else if (opcion == 2) {
+                System.out.println("Ingrese texto a ingresar en la pila: ");
+
+                pila.push(sc.next());
+
+            } else if (opcion == 3) {
+                System.out.println("Sacando ...");
+                System.out.println(pila.pop());
+            }
+        }
 
 
     }
